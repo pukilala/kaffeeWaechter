fdbbd6fe7da726f5a83f401ed23223e3b6f14430
PukiRuby <t@bmi.de>
2018-02-26 21:01:34 +0100

    ohne close and oen

--- DB_Connection.py
+++ DB_Connection.py
@@ -4,6 +4,11 @@ class DB_Connection(object):
 
     connection = None
 
+
+    def __init__(self):
+        self.connection = sqlite3.connect("kaffeeWaechter.db")    
+
+
     def _open(self):
         self.connection = sqlite3.connect("kaffeeWaechter.db")
 
@@ -11,17 +16,14 @@ class DB_Connection(object):
         self.connection.close()
 
     def executeQuery(self, query):
-        self._open()
         cursor = self.connection.cursor()
         cursor.execute(query)
         resultTable = cursor.fetchall()
-        self._close()
+
 
         return resultTable
 
     def executeNonQuery(self, query):
-        self._open()
         cursor=self.connection.cursor()
         cursor.execute(query)
         self.connection.commit()
-        self._close()
